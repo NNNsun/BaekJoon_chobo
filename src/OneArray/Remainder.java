684ed639242d6f168bc11ac32b876605ed0e74ea
sun <niketjssun31@gmail.com>
2022-02-04 23:16:08 +0900

    i'm so angry!

--- src/OneArray/Remainder.java
+++ src/OneArray/Remainder.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 
 public class Remainder {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		System.out.println("½ÇÇà");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		HashSet<Integer> h =new HashSet<Integer>();
 		for(int i=0; i<10; i++) {
